208604ba78058203eaf56a10f22d8e028bc94b01
syaiful919 <syaiful919@gmail.com>
2021-08-11 13:33:47 +0700

    Initial Commit

---
+++ GameCatalogue/GameCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogueApp.swift
+//  GameCatalogue
+//
+//  Created by Syaiful Salam on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GameCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
